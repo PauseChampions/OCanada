0498145362d701adb249cb89b6c95262d1d54793
Rithik Bhatia <rithikbhatia12@gmail.com>
2021-06-18 19:38:46 -0400

    Add project files.

---
+++ OCanada/Installers/OCanadaMenuInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace OCanada.Installers
+{
+    class OCanadaMenuInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
