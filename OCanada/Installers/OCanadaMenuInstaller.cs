737e9625365ab34209a4ece2906dfe7ed3c9f655
Skalx <alexisroy@hotmail.ca>
2021-06-18 22:32:35 -0400

    Add Game UI
    (this is where we beat the uk)

--- OCanada/Installers/OCanadaMenuInstaller.cs
+++ OCanada/Installers/OCanadaMenuInstaller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OCanada.UI.ViewControllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace OCanada.Installers
     {
         public override void InstallBindings()
         {
+            Container.BindInterfacesTo<OCanadaGameController>().AsSingle();
         }
     }
 }
---
+++ OCanada/UI/ViewControllers/OCanadaGameController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.GameplaySetup;
+using BeatSaberMarkupLanguage.ViewControllers;
+using Zenject;
+
+namespace OCanada.UI.ViewControllers
+{
+    internal class OCanadaGameController : IInitializable, IDisposable, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [UIValue("clickable-images")]
+        private List<object> clickableImages = Enumerable.Range(1, 24).Select(i =>
+        {
+            return new ClickableImage() as object;
+        }).ToList();
+        public void Initialize()
+        {
+            GameplaySetup.instance.AddTab("O Canada", "OCanada.UI.Views.OCanadaGame.bsml", this);
+        }
+
+        public void Dispose()
+        {
+            GameplaySetup.instance?.RemoveTab("O Canada");
+        }
+
+    }
+}
