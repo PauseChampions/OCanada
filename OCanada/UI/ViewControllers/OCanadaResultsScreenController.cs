31318b62a021f17fcdd77c735303888b1c7949c9
Sabooboo <smjdunne@gmail.com>
2021-07-04 04:06:51 -0300

    partial implementation of result screen jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj

---
+++ OCanada/UI/ViewControllers/OCanadaResultsScreenController.cs
@@ -0,0 +1,129 @@
+﻿using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Parser;
+using HMUI;
+using IPA.Utilities;
+using OCanada.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace OCanada.UI
+{
+    class OCanadaResultsScreenController : IInitializable, IDisposable, INotifyPropertyChanged
+    {
+        public event Action DoneClicked;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [UIParams]
+        private readonly BSMLParserParams parserParams;
+
+        private bool parsed;
+        private Mode selectedMode;
+
+        GameplaySetupViewController gameplaySetupViewController;
+
+        [UIComponent("root")]
+        private readonly RectTransform rootTransform;
+
+        [UIComponent("modal")]
+        private ModalView modalView;
+
+        [UIComponent("modal")]
+        private readonly RectTransform modalTransform;
+
+        private Vector3 modalPosition;
+
+        private int _currentScore;
+        private int CurrentScore
+        {
+            get => _currentScore;
+            set
+            {
+                _currentScore = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentScoreFormatted)));
+            }
+        }
+        [UIValue("current-score")]
+        private string CurrentScoreFormatted => $"Score: {CurrentScore}";
+
+        [UIValue("high-score")]
+        private string HighScoreFormatted
+        {
+            get
+            {
+                if (selectedMode == Mode.Standard)
+                {
+                    return $"High Score: {PluginConfig.Instance.HighScoreStandard}";
+                }
+                else if (selectedMode == Mode.Endless)
+                {
+                    return $"High Score: {PluginConfig.Instance.HighScoreEndless}";
+                }
+                else
+                {
+                    return ""; // ah yes.
+                }
+            }
+        }
+
+        public OCanadaResultsScreenController(GameplaySetupViewController gameplaySetupViewController)
+        {
+            this.gameplaySetupViewController = gameplaySetupViewController;
+        }
+        public void Initialize()
+        {
+            gameplaySetupViewController.didDeactivateEvent += GameplaySetupViewController_didDeactivateEvent;
+            parsed = false;
+        }
+
+        private void GameplaySetupViewController_didDeactivateEvent(bool firstActivation, bool addedToHierarchy)
+        {
+            if (parsed && rootTransform != null && modalTransform != null)
+            {
+                modalTransform.SetParent(rootTransform);
+                modalTransform.gameObject.SetActive(false);
+            }
+        }
+
+        public void Dispose()
+        {
+            gameplaySetupViewController.didDeactivateEvent -= GameplaySetupViewController_didDeactivateEvent;
+        }
+
+        [UIAction("done-pressed")]
+        private void ExitButtonPressed()
+        {
+            parserParams.EmitEvent("close-modal");
+            DoneClicked?.Invoke();
+        }
+
+
+        private void Parse(Transform parentTransform)
+        {
+            if (!parsed)
+            {
+                BSMLParser.instance.Parse(Utilities.GetResourceContent(Assembly.GetExecutingAssembly(), "OCanada.UI.Views.OCanadaResultsScreen.bsml"), parentTransform.gameObject, this);
+                modalPosition = modalTransform.position;
+                parsed = true;
+            }
+            modalTransform.position = modalPosition;
+            FieldAccessor<ModalView, bool>.Set(ref modalView, "_animateParentCanvas", true);
+        }
+
+        internal void ShowModal(Transform parentTransform, Mode selectedMode, int score)
+        {
+            Parse(parentTransform);
+            parserParams.EmitEvent("close-modal");
+            parserParams.EmitEvent("open-modal");
+            this.selectedMode = selectedMode;
+            CurrentScore = score;
+        }
+    }
+}
