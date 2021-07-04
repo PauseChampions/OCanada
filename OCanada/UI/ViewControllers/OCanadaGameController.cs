5f704bd47508be3b27cf7c86a8dd4c71669ebc71
PixelBoom <rithikbhatia12@gmail.com>
2021-07-04 00:12:28 -0400

    Added Gameplay Logic
    - Random Flag spawning
    - Flag disappear on click
    - Score modify on click

--- OCanada/UI/ViewControllers/OCanadaGameController.cs
+++ OCanada/UI/ViewControllers/OCanadaGameController.cs
@@ -14,13 +14,19 @@ namespace OCanada.UI
 {
     internal class OCanadaGameController : MonoBehaviour, IInitializable, IDisposable, INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        private OCanadaPauseMenuController oCanadaPauseMenuController;
+
         private bool parsed;
         private Mode selectedMode;
-        private OCanadaPauseMenuController oCanadaPauseMenuController;
+        private int score;
+
         private int prevUpdate;
         private float currentTime;
         private readonly float SPAWN_TIME = 5000;
+
+        private System.Random random;
+
+        public event PropertyChangedEventHandler PropertyChanged;
         public event Action GameExit;
 
         [UIComponent("root")]
@@ -43,8 +49,10 @@ namespace OCanada.UI
             parsed = false;
             gameObject.SetActive(false);
             selectedMode = Mode.None;
+            score = 0;
             prevUpdate = 1;
             currentTime = SPAWN_TIME;
+            random = new System.Random();
             oCanadaPauseMenuController.ExitClicked += OCanadaPauseMenuController_ExitClicked;
         }
 
@@ -70,9 +78,14 @@ namespace OCanada.UI
             Parse(siblingTransform);
             rootTransform.gameObject.SetActive(true);
             this.selectedMode = selectedMode;
-            FlagImage flagImage = new FlagImage("OCanada.Images.Canada.png");
-            flagImage.SpriteLoaded += FlagImage_SpriteLoaded;
-            _ = flagImage.Sprite;
+            score = 0;
+
+            foreach (var clickableFlag in clickableImages.OfType<ClickableFlag>())
+            {
+                clickableFlag.FlagClickedEvent += ClickableFlag_FlagClickedEvent;
+            }
+
+            SpawnFlags();
         }
 
         public void Update()
@@ -81,34 +94,64 @@ namespace OCanada.UI
             {
                 var currentUpdate = Mathf.FloorToInt(Time.time * 1000);
                 var difference = currentUpdate - prevUpdate; //Calculate difference in time
-                SpawnFlags(difference); //Pass difference to flash image
                 prevUpdate = currentUpdate;
             }
         }
 
-        private void SpawnFlags(int difference)
+        private void SpawnFlags()
         {
-            currentTime -= difference;
-            if (currentTime <= 0)
+            for (int i = 0; i < 7; i++)
             {
-                currentTime = SPAWN_TIME;
+                SpawnFlag();
+            }
+        }
+
+        private void SpawnFlag()
+        {
+            List<ClickableFlag> imageList = clickableImages.OfType<ClickableFlag>().ToList();
+            int canadianFlag = random.Next(1, 6);
+            int imageIndex = random.Next(imageList.Count);
+            int flagIndex = 0;
+            if (canadianFlag != 5)
+            {
+                flagIndex = random.Next(Flags.FlagList.Count);
+            }
+            imageList[imageIndex].SetImage(Flags.FlagList[flagIndex]);
+        }
+
+        private void ClearFlags()
+        {
+            foreach (var clickableFlag in clickableImages.OfType<ClickableFlag>())
+            {
+                clickableFlag.SetImage(null);
+            }
+        }
+
+        private void ClickableFlag_FlagClickedEvent(int pointValue)
+        {
+            score += pointValue;
+            bool respawn = true;
+
+            foreach (var clickableFlag in clickableImages.OfType<ClickableFlag>())
+            {
+                if (clickableFlag.PointValue > 0)
+                {
+                    respawn = false;
+                }
+            }
+
+            if (respawn)
+            {
+                ClearFlags();
+                SpawnFlags();
             }
         }
 
         private void ExitGame()
         {
-            // TODO: exit game pog
-        }
-
-        private void FlagImage_SpriteLoaded(object sender, System.EventArgs e)
-        {
-            if (sender is FlagImage flagImage)
+            foreach (var clickableFlag in clickableImages.OfType<ClickableFlag>())
             {
-                foreach (var clickableFlag in clickableImages.OfType<ClickableFlag>())
-                {
-                    clickableFlag.clickableImage.sprite = flagImage.Sprite;
-                }
-                flagImage.SpriteLoaded -= FlagImage_SpriteLoaded;
+                clickableFlag.FlagClickedEvent -= ClickableFlag_FlagClickedEvent;
             }
         }
 
@@ -122,6 +165,7 @@ namespace OCanada.UI
         private void OCanadaPauseMenuController_ExitClicked()
         {
             rootTransform.gameObject.SetActive(false);
+            ExitGame();
             GameExit?.Invoke();
         }
     }
@@ -129,6 +173,56 @@ namespace OCanada.UI
     internal class ClickableFlag
     {
         [UIComponent("clickable-image")]
-        internal ClickableImage clickableImage;
+        private ClickableImage clickableImage;
+
+        private FlagImage flagImage;
+        internal event Action<int> FlagClickedEvent;
+
+        internal int PointValue => flagImage != null ? flagImage.PointValue : 0;
+
+        internal void SetImage(FlagImage flagImage)
+        {
+            if (flagImage != null)
+            {
+                if (flagImage.SpriteWasLoaded)
+                {
+                    clickableImage.sprite = flagImage.Sprite;
+                    this.flagImage = flagImage;
+                }
+                else
+                {
+                    this.flagImage = null;
+                    flagImage.SpriteLoaded += FlagImage_SpriteLoaded;
+                    _ = flagImage.Sprite;
+                }
+            }
+            else
+            {
+                clickableImage.sprite = Utilities.ImageResources.BlankSprite;
+                this.flagImage = null;
+            }
+        }
+
+        private void FlagImage_SpriteLoaded(object sender, System.EventArgs e)
+        {
+            if (sender is FlagImage flagImage)
+            {
+                this.flagImage = flagImage;
+                clickableImage.sprite = flagImage.Sprite;
+                flagImage.SpriteLoaded -= FlagImage_SpriteLoaded;
+            }
+        }
+
+        [UIAction("flag-clicked")]
+        private void FlagClicked()
+        {
+            if (flagImage != null)
+            {
+                int pointValue = PointValue;
+                flagImage = null;
+                clickableImage.sprite = Utilities.ImageResources.BlankSprite;
+                FlagClickedEvent?.Invoke(pointValue);
+            }
+        }
     }
 }
