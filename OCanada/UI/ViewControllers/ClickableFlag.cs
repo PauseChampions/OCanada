b8127af92bd7f32ac9621ee2f03f10945211f611
Sabooboo <74437615+Sabooboo@users.noreply.github.com>
2022-02-12 20:49:59 -0400

    extract ClickableFlag from to separate file

---
+++ OCanada/UI/ViewControllers/ClickableFlag.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using HMUI;
+using IPA.Utilities;
+using UnityEngine;
+
+namespace OCanada.UI
+{
+    internal class ClickableFlag
+    {
+        [UIComponent("clickable-image")]
+        private ClickableImage clickableImage;
+        internal event Action<int> FlagClickedEvent;
+
+        internal int PointValue;
+
+        internal void SetImage(Sprite flagImage, int pointValue = 0)
+        {
+            PointValue = pointValue;
+            if (flagImage == null)
+            {
+                clickableImage.sprite = Utilities.ImageResources.BlankSprite;
+                return;
+            }
+            
+            clickableImage.sprite = flagImage;
+            clickableImage.HighlightColor = PointValue >= 5
+                ? Color.yellow
+                : Color.red;
+        }
+
+        [UIAction("#post-parse")]
+        private void PostParse()
+        {
+            FieldAccessor<ImageView, float>.Set(clickableImage, "_skew", 0);
+        }
+
+        [UIAction("flag-clicked")]
+        private void FlagClicked()
+        {
+            int pointValue = PointValue;
+            PointValue = 0;
+            clickableImage.sprite = Utilities.ImageResources.BlankSprite;
+            FlagClickedEvent?.Invoke(pointValue);
+        }
+    }
+}
--- OCanada/UI/ViewControllers/OCanadaGameController.cs
+++ OCanada/UI/ViewControllers/OCanadaGameController.cs
@@ -5,9 +5,6 @@ using System.Linq;
 using System.Reflection;
 using BeatSaberMarkupLanguage;
 using BeatSaberMarkupLanguage.Attributes;
-using BeatSaberMarkupLanguage.Components;
-using HMUI;
-using IPA.Utilities;
 using OCanada.Configuration;
 using OCanada.GameplaySetupScene;
 using OCanada.Flag;
@@ -349,44 +346,6 @@ namespace OCanada.UI
         private void GameplaySetupViewController_didDeactivateEvent(bool removedFromHierarchy, bool screenSystemDisabling)
         {
             ExitGame();
-        }
-    }
-    internal class ClickableFlag
-    {
-        [UIComponent("clickable-image")]
-        private ClickableImage clickableImage;
-        internal event Action<int> FlagClickedEvent;
-
-        internal int PointValue;
-
-        internal void SetImage(Sprite flagImage, int pointValue = 0)
-        {
-            PointValue = pointValue;
-            if (flagImage == null)
-            {
-                clickableImage.sprite = Utilities.ImageResources.BlankSprite;
-                return;
-            }
-            
-            clickableImage.sprite = flagImage;
-            clickableImage.HighlightColor = PointValue >= 5
-                ? Color.yellow
-                : Color.red;
-        }
-
-        [UIAction("#post-parse")]
-        private void PostParse()
-        {
-            FieldAccessor<ImageView, float>.Set(clickableImage, "_skew", 0);
-        }
-
-        [UIAction("flag-clicked")]
-        private void FlagClicked()
-        {
-            int pointValue = PointValue;
-            PointValue = 0;
-            clickableImage.sprite = Utilities.ImageResources.BlankSprite;
-            FlagClickedEvent?.Invoke(pointValue);
         }
     }
 }
